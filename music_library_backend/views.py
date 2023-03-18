61ca1f66bde305d044388fd91830e47463c688b2
Ruth Hermosillo <hermosilloruth59@gmail.com>
2023-03-17 21:06:46 -0600

    Create New Views Functions for Getting Songs by Id, Updating Songs and Deleting Songs + Add New URLs Path for song_details Function.

--- music_library_backend/views.py
+++ music_library_backend/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .serializers import SongSerializer
 from .models import Song
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 @api_view(['GET', 'POST'])
@@ -20,3 +21,18 @@ def songs_list(request):
             return Response(serializer.data, status= status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
+def song_details(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.methos == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
